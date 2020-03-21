1cc382f3c846a4c795631c7d95b39b70199d776e
ijuresa <ivanjuresa611@gmail.com>
2020-03-21 15:26:06 +0100

    MASTER: Added RCC initialisation. Tested.

---
+++ scaBL/APP/main.c
@@ -0,0 +1,86 @@
+/***********************************************************************************************//**
+ * MIT License
+ * 
+ * Copyright (c) 2020 ivan.juresa
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ **************************************************************************************************
+ * @file   main.c
+ * @author ivan.juresa
+ * @brief  Main file for scaBL.
+ **************************************************************************************************/
+
+#ifndef MAIN_C_
+#define MAIN_C_
+
+/***************************************************************************************************
+ *                      INCLUDE FILES
+ **************************************************************************************************/
+#include "typedefs.h"
+
+// DRV
+#include "drv_rcc.h"
+
+/***************************************************************************************************
+ *                      PRIVATE DEFINES
+ **************************************************************************************************/
+
+/***************************************************************************************************
+ *                      PRIVATE DATA TYPES
+ **************************************************************************************************/
+
+/***************************************************************************************************
+ *                      PRIVATE VARIABLES
+ **************************************************************************************************/
+
+/***************************************************************************************************
+ *                      GLOBAL VARIABLES DEFINITION
+ **************************************************************************************************/
+
+/***************************************************************************************************
+ *                      PRIVATE FUNCTION DECLARATION
+ **************************************************************************************************/
+static void DRV_init(void);
+
+/***************************************************************************************************
+ *                      PUBLIC FUNCTIONS DEFINITION
+ **************************************************************************************************/
+int main(void) {
+    /* Initialization procedure */
+    // DRV
+    DRV_init();
+
+    while(TRUE);
+
+    return 0;
+}
+
+/***************************************************************************************************
+ *                      PRIVATE FUNCTIONS DEFINITION
+ **************************************************************************************************/
+static void DRV_init(void) {
+    // Clock
+    DRV_RCC_init();
+}
+
+#endif // MAIN_C_
+
+/***************************************************************************************************
+ *                      END OF FILE
+ **************************************************************************************************/
---
+++ scaBL/DRV/drv_rcc.c
@@ -0,0 +1,110 @@
+/***********************************************************************************************//**
+ * MIT License
+ * 
+ * Copyright (c) 2020 ivan.juresa
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ **************************************************************************************************
+ * @file   drv_rcc.c
+ * @author ivan.juresa
+ * @brief  Reset and Clock Control driver for STM32F42xxx.
+ **************************************************************************************************/
+
+#ifndef DRV_RCC_C_
+#define DRV_RCC_C_
+
+/***************************************************************************************************
+ *                      INCLUDE FILES
+ **************************************************************************************************/
+// DRV
+#include "drv_rcc.h"
+
+/***************************************************************************************************
+ *                      PRIVATE DEFINES
+ **************************************************************************************************/
+
+/***************************************************************************************************
+ *                      PRIVATE DATA TYPES
+ **************************************************************************************************/
+
+/***************************************************************************************************
+ *                      PRIVATE VARIABLES
+ **************************************************************************************************/
+
+/***************************************************************************************************
+ *                      GLOBAL VARIABLES DEFINITION
+ **************************************************************************************************/
+
+/***************************************************************************************************
+ *                      PRIVATE FUNCTION DECLARATION
+ **************************************************************************************************/
+
+/***************************************************************************************************
+ *                      PUBLIC FUNCTIONS DEFINITION
+ **************************************************************************************************/
+void DRV_RCC_init(void) {
+    /*
+     * Configure PPL before enabling HSE as if we enable HSE first PPL will go to ready state
+     * and we won't be able to configure division factors.
+     * */
+    // Turn OFF PPL so it can be configured
+    RCC->CR &= ~RCC_CR_PLLON;
+
+    // Configure PPL
+    RCC->PLLCFGR |= (RCC_PLLCFGR_PLLSRC_HSE |
+                     RCC_PLLCFGR_PLLN_3     |
+                     RCC_PLLCFGR_PLLN_5     |
+                     RCC_PLLCFGR_PLLN_7     |
+                     RCC_PLLCFGR_PLLM_2     |
+                     RCC_PLLCFGR_PLLQ_0     |
+                     RCC_PLLCFGR_PLLQ_1     |
+                     RCC_PLLCFGR_PLLQ_2);
+
+    // Enable HSE
+    RCC->CR |= RCC_CR_HSEON;
+
+    // Check if HSE is stable
+    while(((RCC->CR & RCC_CR_HSEON) >> RCC_CR_HSEON_Pos) != TRUE);
+
+    // Configure Clock
+    RCC->CFGR |= RCC_CFGR_SW_HSE | RCC_CFGR_PPRE1_DIV4 | RCC_CFGR_PPRE2_DIV2;
+
+    // Enable PPL
+    RCC->CR |= RCC_CR_PLLON;
+
+    // Wait for PPL to lock
+    while(((RCC->CR & RCC_CR_PLLRDY) >> RCC_CR_PLLRDY_Pos) != TRUE);
+
+    // Select PPL as System Clock
+    RCC->CFGR = (((RCC->CFGR & (~RCC_CFGR_SW)) | RCC_CFGR_SW_PLL));
+
+    // Wait for HW to indicate that PPL is used as System Clock
+    while((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_PLL);
+}
+
+
+/***************************************************************************************************
+ *                      PRIVATE FUNCTIONS DEFINITION
+ **************************************************************************************************/
+
+#endif // DRV_RCC_C_
+
+/***************************************************************************************************
+ *                      END OF FILE
+ **************************************************************************************************/
